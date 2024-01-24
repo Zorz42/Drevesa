1586a2b4f1a3a4d851a8ff59a2495cad3b05f95d
Jakob Zorz <54270248+Zorz42@users.noreply.github.com>
2024-01-24 22:02:32 +0100

    malo shitpostanja

--- src/page4.php
+++ src/page4.php
@@ -18,6 +18,51 @@
     
     <img src="static/piwo.gif" alt="naj vam tekne pivo">
 
+    <p>
+        Izberite si svoj najljubši programski jezik:
+    </p>
+
+    <form action="https://www.google.com/search" method="get">
+        <input type="radio" name="q" value="c++" checked> C++
+        <input type="radio" name="q" value="c"> C
+        <input type="radio" name="q" value="go"> Go
+        <input type="radio" name="q" value="rust"> Rust
+        <input type="radio" name="q" value="haskell"> Haskell
+        <input type="radio" name="q" value="assembly"> Assembly
+        <input type="radio" name="q" value="swift"> Swift
+    </form>
+
+    <p style="font-size: 10px">
+        Na voljo so samo jeziki, ki so vredni svojega imena.
+    </p>
+
+    Tukaj je tabela z vsemi številkami, ki so zanimive:
+
+    <table style="border: 1px solid #dddddd">
+        <tr>
+            <th>2</th>
+            <th>3</th>
+            <th>5</th>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>11</td>
+            <td>13</td>
+        </tr>
+        <tr>
+            <td>17</td>
+            <td>19</td>
+            <td>23</td>
+        </tr>
+    </table>
+
+    <a href="https://www.youtube.com/watch?v=gDjMZvYWUdo" style="width: 200%"><img src="static/saul.png" alt="saul goodman"></a>
+
+    <p style="color: red">
+        Nisem nor, samo zahteve za projekt pri informatiki so precej nenavadne in ne vem, kaj naj naredim.
+    </p>
+
+
 </main>
 
 </body>
